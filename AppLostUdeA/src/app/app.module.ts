1616af94c7bb431a5a7f840a325260c5e600a2d3
veluaru <angie.castaneda@udea.edu.co>
2020-01-15 22:11:33 -0500

    login y home responsive

--- AppLostUdeA/src/app/app.module.ts
+++ AppLostUdeA/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { EncontreObjetoComponent } from './encontreObjeto/encontreObjeto.component';
 import { BuscarObjetoComponent } from './buscarObjeto/buscarObjeto.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
    declarations: [
@@ -18,7 +19,8 @@ import { BuscarObjetoComponent } from './buscarObjeto/buscarObjeto.component';
    ],
    imports: [
       BrowserModule,
-      AppRoutingModule
+      AppRoutingModule,
+      MDBBootstrapModule.forRoot()
    ],
    providers: [],
    bootstrap: [
